5f44780d41bc062504fe5c8c1cb0d902fd3e8d22
Rebekah Keating Godfrey <115112401+rkeatinggodfrey@users.noreply.github.com>
2023-09-26 19:12:55 -0400

    Add files via upload

---
+++ fastqc.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH --job-name=fastqc_rna	# Job name that will shop up in queue
+#SBATCH --output=fastqc_%j.out	# Running + error log output
+#SBATCH --mail-type=END,FAIL	 # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=your.email@ufl.edu>	# Your email address for notifications
+#SBATCH --nodes=1		# Use one node
+#SBATCH --ntasks=1		# Run a single task
+#SBATCH --cpus-per-task=4	# number of cpus
+#SBATCH --mem=4gb		# memory
+#SBTACH -t 10:00:00 		# time limit in d-hh:mm:ss
+
+
+module load fastqc
+fastqc file.R1.fq file.R2.fq
