b581ae8a5312895fc440dd619f68e5df3452bd9b
rkeatinggodfrey <rkeatinggodfrey@gmail.com>
2023-09-26 22:42:39 -0400

    trim updates

---
+++ 02_Trim/trimmomatic.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=trim_{identifier}
+#SBATCH --output=trim_%j.log
+#SBATCH --mail-user=your.email@ufl.edu
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mem-per-cpu=8gb
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+
+module load trimmomatic/0.39
+
+
+trimmomatic PE -threads 4 \
+{Forward_Read_R1}.fastq.gz {Reverse_Read_R2}.fastq.gz \
+trimmed_{Forward_Read_R1}.fastq.gz trimmed_unparied_{Forward_Read_R1}.fastq.gz \
+trimmed_{Reverse_Read_R2}_001.fastq.gz trimmed_unpaired_{Reverse_Read_R2}.fastq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2
+:True LEADING:3 TRAILING:3 MINLEN:36
