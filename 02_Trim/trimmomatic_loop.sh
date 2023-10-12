d5b31b5486dfb2154567406257f30c4fc80dd521
rkeatinggodfrey <rkeatinggodfrey@gmail.com>
2023-10-12 11:07:17 -0400

    trim update

---
+++ 02_Trim/trimmomatic_loop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=trimm
+#SBATCH --output=%x_%j.log
+#SBATCH --mail-user={your.name}@ufl.edu
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mem-per-cpu=4gb
+#SBATCH --time=48:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+
+module load trimmomatic/0.39
+
+for sample in $(ls *fastq.gz | cut -d "_" -f 1,2,3 | sort | uniq)
+do
+    fq1=$(ls ${sample}_R1*)
+    fq2=$(ls ${sample}_R2*)
+    trimmomatic PE -threads 16 \
+    ${fq1} ${fq2} \
+    ${sample}_R1_clean.fastq.gz ${sample}_R1_unpaired.fastq.gz \
+    ${sample}_R2_clean.fastq.gz ${sample}_R2_unpaired.fastq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2 LEADING:3 TRAILING:3 MINLEN:36
+done
