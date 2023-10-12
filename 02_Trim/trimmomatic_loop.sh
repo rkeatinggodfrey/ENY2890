8e1feb98d92a081413594b2944b529dfb2bdeeb3
Rebekah Keating Godfrey <115112401+rkeatinggodfrey@users.noreply.github.com>
2023-10-12 11:24:00 -0400

    Update trimmomatic_loop.sh

--- 02_Trim/trimmomatic_loop.sh
+++ 02_Trim/trimmomatic_loop.sh
@@ -14,7 +14,7 @@ for sample in $(ls *fastq.gz | cut -d "_" -f 1,2,3 | sort | uniq)
 do
     fq1=$(ls ${sample}_R1*)
     fq2=$(ls ${sample}_R2*)
-    trimmomatic PE -threads 16 \
+    trimmomatic PE -threads 4 \
     ${fq1} ${fq2} \
     ${sample}_R1_clean.fastq.gz ${sample}_R1_unpaired.fastq.gz \
     ${sample}_R2_clean.fastq.gz ${sample}_R2_unpaired.fastq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2 LEADING:3 TRAILING:3 MINLEN:36
