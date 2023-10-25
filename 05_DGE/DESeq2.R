1847fadb4f059e5962b7be37fc82f73f1403b23a
rkeatinggodfrey <rkeatinggodfrey@gmail.com>
2023-10-25 10:40:14 -0400

    dge update

--- 05_DGE/DESeq2.R
+++ 05_DGE/DESeq2.R
@@ -7,10 +7,9 @@
 ## https://people.duke.edu/~ccc14/duke-hts-2017/Statistics/08032017/DESeq2-Notebook-introduction.html
 ## https://www.rdocumentation.org/packages/DESeq2/versions/1.12.3/topics/DESeqDataSet-class
 
-
+## Install the packages you need to do the analysis
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-
 BiocManager::install("DESeq2")
 
 library(DESeq2)
@@ -19,3 +18,50 @@ library(ggplot2)
 
 install.packages("htmltools")
 library(htmltools)
+
+##############################################################
+### (1) Read in counts data and combine into one dataframe ###
+##############################################################
+## Create an object out of the filepath to the count .tsv files
+
+## change working director to location of counts files
+## you can also go to Session > Set working directory > Source file location
+## to set directory to the folder where this R script is saved
+setwd("{filepath to folder on your computer with your counts.tsv files}")
+
+## create a list object that contains all of the .tsv files in this directory
+filedir <-list.files(pattern = ".tsv$")
+
+## Import all files as a list of dataframes
+all_files <- lapply(filedir,function(x) {
+  read.table(file = x, 
+             sep = '\t', 
+             header = F,
+             row.names=1)
+})
+
+## Combine into single dataframe
+counts <-bind_cols(all_files)
+
+## create a string of file names
+file.names<-c(filedir[1], filedir)
+
+## why does it duplicate the first file name? Remove it!
+file.names <- file.names[2:26]
+
+## now get rid of the ".tsv" in the file name
+file.names <- gsub(".tsv", "", file.names)
+
+## name the columns of your counts dataframe to match your metadata file
+colnames(counts)<-c(file.names)
+
+## change directory back to the one where your metadata is located
+## you can also go to Session > Set working directory > Source file location
+## to set directory to the folder where this R script is saved
+setwd("/Users/rkeatinggodfrey/Documents/2023/03_CURE/DESeq2")
+
+## we used the geneIDs as row names for importing and combining files,
+## but now we need them to be a column for our analysis
+## so we need to turn the gene id row names into a column (variable)
+counts <-tibble::rownames_to_column(counts,"gene_id")
+
--- 05_DGE/readme.md
+++ 05_DGE/readme.md
@@ -3,7 +3,7 @@
 Now that we have (1) gene counts for each set of reads for each sample and (2) a metadata file that gives us biological conditions (sex, body part) for each sample, we can use statistical analyses to ask if particular genes were detected more frequently in certain samples or in certain biological conditions. 
   
 
-### DGE DESeq2  
+### (1) Download R and R studio to run DESeq2 
 
 To analyze DGE, will use a program written in the R language called DESeq2.  
 
@@ -16,3 +16,9 @@ If you want to run this analysis locally on your own computer, you will need to 
 
 This is a useful guide for working in RStudio
 + [Hands-On Programming with R github](https://rstudio-education.github.io/hopr/starting.html)
+
+
+### (2) Download counts (.tsv) files from HiPer Gator  
+
+At this point everyone needs a copy of all counts files. Download your counts files and share copies with your group members!
+
