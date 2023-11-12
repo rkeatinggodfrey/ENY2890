fae796253197b911189b799c29a68122adb2b0bc
rkeatinggodfrey <rkeatinggodfrey@gmail.com>
2023-11-11 20:10:54 -0500

    DESeq2.R update

--- 05_DGE/DESeq2.R
+++ 05_DGE/DESeq2.R
@@ -1,6 +1,7 @@
+
 ## Project: Fall 2023 CURE Course 
 ## Authors: R. Keating Godfrey
-## Last updated: 23-11-08
+## Last updated: 23-11-11
 
 ## Resources:
 ## https://bioconductor.org/packages/release/bioc/html/DESeq2.html
@@ -20,7 +21,6 @@ library(ggplot2)
 
 install.packages("htmltools")
 library(htmltools)
-
 
 
 
@@ -99,56 +99,74 @@ colnames(meta) <-c("sample","sex","body_part")
 meta$sex <-as.factor(meta$sex)
 meta$body_part <-as.factor(meta$body_part)
 
-## check that they are factors
+## check that they are factors (this should return "TRUE")
 is.factor(meta$sex)
 is.factor(meta$body_part)
 
 
 ### (3) Set up DESeq2 Model and Run analysis ###
   
-### Construct a DESeq data set object ###
 
 ## In this example I am comparing genitalia from males and 
 ## females, so I just need to designate comparisons by sex (~sex)
+## If you are comparing both body parts and sex, include 
+## design=body_part+sex
+## To learn what the different variables of DESeqDataSetFromMatrix
+## are, run:
+?DESeqDataSetFromMatrix
+
+### Construct a DESeq data set object ###
+## This object stores all of the input data needed to run differential
+## expression analysis
 dds.gen <-DESeqDataSetFromMatrix(countData=counts, # the counts data object
                              colData=meta, # the meta data
                              design=~sex, # the statistical design 
                              tidy=T)
-### Run DESeq analyis ###
+### Run DESeq analyis using the DESeq function###
 dds.gen <- DESeq(dds.gen)
 
-## Get the results 
-results.gen <-results(dds.gen)
+## Get a table of the results 
+## This is going to summarize the findings into a table
+## And pick out the specific comparison you are trying to make
+## Remember in this example data set I only have genitalia samples
+## so I want the results to look at the variable "sex"
+## and the levels "males" and "females"
+results.gen <-results(dds.gen, contrast=c("sex","Male","Female"))
 
 ## Look at summary of analysis
+## This will tell you how many genes showed 
+## a positive difference in expression (LFC = log fold change)
+## a negative difference in expression 
+## in my case in Males vs Females as stated above
 summary(results.gen)
-
-## Look at the top of the results file
-head(results.gen(dds,tidy=T))
 
 ## Look at the top of the results file ordered by adjusted p-value
 results.gen <- results.gen[order(results.gen$padj),]
 head(results.gen)
 
-
-## Get results based on two factor levels
-results.mvf <-results(dds.gen, contrast=c("sex","Male","Female"))
-
-## Look at summary of analysis 
-## (this will be the same as when you didn't use "contrast")
-summary(results.mvf)
-
-## Look at the top of the results file ordered by adjusted p-value
-results.mvf <- results.mvf[order(results.mvf$padj),]
-head(results.mvf)
+## You can save this results file as a csv in your current working directory
+write.csv(results.gen,"Genitalia_Males_v_Females_Results_table_01.csv")
 
 
-## (4) Merge results with functional annotation
+
+### (4) Merge results with functional annotation ###
+
+## To run this part of the script you need a file with 
+## a functional annotation of the genome
+## you can copy this file from the shared folder on HiperGator
+## /blue/eny2890/share/05_dge/rna.annot.cds.emapper.annotations
+## then download it, open in excel and save as .csv 
+## or download it in csv format from our github
+
+
+## First, let's get out gene IDs into column names because
+## we need to merge the DESeq2 results with the 
+## functional annotation based on those names
 
 ## The geneIDs are the row names in the original results table
 ## turn them into a column to merge with functional annotation from eggnog 
 library(tibble)
-results.table <- as.data.frame(results.mvf)
+results.table <- as.data.frame(results.gen)
 results.table <- tibble::rownames_to_column(results.table,"geneID")
 
 ## read in the annotation file
@@ -157,6 +175,11 @@ annot <- read.csv("rna.annot.cds.emapper.annotations.geneID.csv", header=T)
 ## merge DGE with annotation file
 results.annotated <-merge(results.table,annot, by=c("geneID"))
 
-## you can write this out as a CSV
-write.csv(results.annotated,"DGE_annotated_test_female_male_geni.csv")
+## you can save this results table that includes
+## DGE and functional annotation information as a CSV
+## in your working directory
+write.csv(results.annotated,"Genitalia_Males_v_Females_Results_table_02.csv")
 
+
+
+################# PROCEED TO FILTERING & VISUALIZATION ################# 
